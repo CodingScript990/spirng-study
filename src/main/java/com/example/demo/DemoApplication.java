da2b6178297416b5303849ccceeccae838af1ae3
CodeScript990 <trte135@naver.com>
2023-06-09 22:32:29 +0900

    feat.June.7.23 Spring[Annotation - Component, Bean]  - Content :    - DeepComponent    - DemoApplication    - FooBarComponent    - NoComponent    - YesComponent    - Annotation의 쓰임새와 Bean에 소속되는 Object를 출력해줘서 소속이 되는 것과 소속이 되지 않는 것을 확인하는 작업을 함

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.yes.very.deep.pack.DeepComponent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,8 +11,11 @@ import org.springframework.context.annotation.ComponentScan;
 // - http://localhost:8080/home -> address를 url에 입력했을 때 우리가 만든 html이 보이게 해보기[clear]
 
 //@SpringBootApplication // Bean Object를 찾아서 관리를 해주겠다는 의미
-@EnableAutoConfiguration
-@ComponentScan(basePackages = "com.example.demo") // com.example.demo 패키지 안에 있는 경로들을 이용하겠다는 의미임
+@EnableAutoConfiguration // springBootApplication 이 동작 할 수 있도록 해주는 역할
+//@ComponentScan(basePackages = "com.example.demo") // com.example.demo 패키지 안에 있는 경로들을 이용하겠다는 의미임
+//@ComponentScan(basePackages = {"com.example.demo.yes", "com.example.demo.controller"}) // 원하는 패키지를 작성하여 사용가능
+@ComponentScan(basePackageClasses = DeepComponent.class) // package 만 불러서 이용이 가능함
+// ComponentScan : 해당 Component 를 사용해주기 위한 것 => Bean
 // IOC : 내가 직접 관리하지 않고, Container 에서 관리를 해준다는 의미
 /**
  * @SpringBootConfiguration : Spring Boot 설정 관련 어노테이션
